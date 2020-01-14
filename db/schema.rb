c631154c29a32ff695f795f898f2adb6d4adad1d
Jenner Thomas <jthomas@jthomas-mbp.local>
2020-01-14 17:49:40 -0500

    add nav and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_214805) do
+ActiveRecord::Schema.define(version: 2020_01_14_224525) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_214805) do
     t.string "director"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
